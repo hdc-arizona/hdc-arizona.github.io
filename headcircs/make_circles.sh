fb26fa3bc75dbcbafee7b157d32e8f0732c0a1e8
Joshua Levine <joshua.a.levine@gmail.com>
2020-01-14 16:39:22 -0700

    making carlos happier

--- headcircs/make_circles.sh
+++ headcircs/make_circles.sh
@@ -1,4 +1,4 @@
 cp ../headshots/*.png .
-convert -fill "#FFFFFF" -size 200x267 xc:none -draw 'circle 99,120 5,120' -blur 5x5 circle.png
+convert -fill "#FFFFFF" -size 200x267 xc:none -draw 'circle 99,120 3,120' -blur 3x3 circle.png
 mogrify -alpha Set -draw 'image Multiply 0,0 0,0 "circle.png"' *.png
 mogrify -crop 200x220+0+10 *.png
