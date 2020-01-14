40c642b1b1848dc99a63ce64d200ec6329441205
Joshua Levine <joshua.a.levine@gmail.com>
2020-01-14 16:04:47 -0700

    Feathered circles

--- headcircs/make_circles.sh
+++ headcircs/make_circles.sh
@@ -1,3 +1,4 @@
 cp ../headshots/*.png .
-convert  -size 200x267 xc:none -draw 'circle 100,120 100,20'  circle.gif
-mogrify -alpha Set -draw 'image Dst_In 0,0 0,0 "circle.gif"' -crop 200x220+0+10 *.png
+convert -fill "#FFFFFF" -size 200x267 xc:none -draw 'circle 99,120 5,120' -blur 5x5 circle.png
+mogrify -alpha Set -draw 'image Multiply 0,0 0,0 "circle.png"' *.png
+mogrify -crop 200x220+0+10 *.png
